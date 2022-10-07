cc1a5be4188568c75890b6f5743922fc41ee51bf
tnk-rin <47429850+tnk-rin@users.noreply.github.com>
2022-10-07 00:34:35 -0700

    Added Audio/Video support

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, send_file
 import downloader
 import os
+import io
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -16,11 +17,31 @@ def download():
     with open('index.html', 'r') as file:
         data = file.read()
     url = request.args.get('url')
+    mode = request.args.get('mode')
     if url != None:
-        downloader.download(url)
+        downloader.download(url, mode)
     return data
 
 @app.route("/file")
 def file():
-    dl = send_file('ytdlout.mp3', as_attachment=True, download_name="converted.mp3")
+    mode = request.args.get('mode')
+    filename = "tmp/ytdlout.mp3" if mode == 'audio' else "tmp/ytdlout.mp4"
+    mime = "audio/mpeg" if mode == 'audio' else "video/mp4"
+    path = os.path.join(os.getcwd(), filename)
+
+    return_data = io.BytesIO()
+    with open(path, 'rb') as fo:
+        return_data.write(fo.read())
+    return_data.seek(0)
+
+    os.remove(path)
+
+    r = send_file(return_data, mimetype=mime, as_attachment=True, attachment_filename="converted.mp3")
+    return r
+
+
+def filer():
+    mode = request.args.get('mode')
+    filename = "tmp/ytdlout.mp3" if mode == 'audio' else "tmp/ytdlout.mp4"
+    dl = send_file(filename, as_attachment=True, download_name="converted.mp3")
     return dl
--- downloader.py
+++ downloader.py
@@ -1,16 +1,23 @@
 from __future__ import unicode_literals
 import yt_dlp
 
-def download(url):
-    ydl_opts = {
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
-            'outtmpl': 'ytdlout.%(ext)s',
-        }
+def download(url, mode):
+    ydl_opts = {}
+    if mode == "audio":
+        ydl_opts = {
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+                'outtmpl': 'tmp/ytdlout.%(ext)s',
+            }
+    elif mode == "video":
+        ydl_opts = {
+                'format': 'mp4+ba/b',
+                'outtmpl': 'tmp/ytdlout.%(ext)s',
+                }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
